0c03ba42e1eea1bb1e4dc9c718dcd4bcb337d1c7
YoussefJell <youssef.jellouli@yahoo.com>
2022-06-27 12:03:43 +0000

    Task 3 & 4

---
+++ models/base_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Base_model module"""
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        kwargc = len(kwargs)
+        if kwargc > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+            self.id = str(uuid4())
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        my_dict = self.__dict__
+        my_dict['__class__'] = __class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
