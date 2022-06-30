a79d07376105e937a4a007b4e77c600c02800c1f
yousseffabid <abidyoussef58@gmail.com>
2022-06-30 14:48:18 +0100

    test5

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from models.review import Review
+from models import *
+import unittest
+import os
+from datetime import datetime
+
+
+class Test_User(unittest.TestCase):
+
+    def test_attributes(self):
+        """Check for attributes."""
+        r = Review()
+        self.assertEqual(str, type(r.id))
+        self.assertEqual(datetime, type(r.created_at))
+        self.assertEqual(datetime, type(r.updated_at))
+        self.assertTrue(hasattr(r, "place_id"))
+        self.assertTrue(hasattr(r, "user_id"))
+        self.assertTrue(hasattr(r, "text"))
+
+    def test_str(self):
+        """test the str output"""
+        r1 = Review()
+        string = f"[{r1.__class__.__name__}] ({r1.id}) {r1.__dict__}"
+        self.assertEqual(str(r1), string)
+
+    def test_save(self):
+        """test if the object is saved in file"""
+        r2 = Review()
+        r2.save()
+        storage.reload()
+        objects = storage.all()
+        self.assertEqual(r2.id, objects[f"{r2.__class__.__name__}.{r2.id}"].id)
+
+    def test_todict(self):
+        """test if the object is converted to dict"""
+        r3 = Review()
+        mydict = r3.to_dict()
+        self.assertEqual(mydict['__class__'], 'Review')
+        self.assertEqual(type(mydict['created_at']), str)
+        self.assertEqual(type(mydict['updated_at']), str)
+
+    @classmethod
+    def tearDownClass(cls):
+        """City testing teardown.
+        Delete the FileStorage
+        """
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+
+
+if __name__ == "__main__":
+    unittest.main()
